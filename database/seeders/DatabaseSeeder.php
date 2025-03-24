e59e4c08c3fe4384d23f6d9dbc1e6632b9dbc191
pat56-hp <patrickkouassi7@gmail.com>
2025-03-24 17:55:00 +0000

    Add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            AdminSeeder::class,
+            SettingSeeder::class,
+            CategorySeeder::class,
+            JourSeeder::class,
+            
+        ]);
     }
 }
