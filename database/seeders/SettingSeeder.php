ccb5ce351ab49c3f1819f272d617896e25311d86
pat56-hp <patrickkouassi7@gmail.com>
2025-03-24 17:31:34 +0000

    Add SettingSeeder

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            "name" => "Les bons plans",
+            "email" => "lesbonsplan@consulting.com",
+            "phone" => "0225070837751",
+            "lien" => "/assets/logo.png",
+            "logo_black" => "/assets/logo.png",
+            "favicon" => "/assets/logo.png",
+            "auteur" => "Patrick Aimé",
+            "Created_by" => "Admin",
+        ];
+
+        \App\Models\Setting::create($data);
+    }
+}
