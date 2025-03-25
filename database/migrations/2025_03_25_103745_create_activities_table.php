5350905cd0c50d3f40ac2b632edbf7b89a6581df
pat56-hp <patrickkouassi7@gmail.com>
2025-03-25 10:38:47 +0000

    create migration activities

---
+++ database/migrations/2025_03_25_103745_create_activities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('admin_id')->constrained('admins')->onDelete('cascade');
+            $table->string('module');
+            $table->text('action');
+            $table->string('ip');
+            $table->string('navigator');
+            $table->string('pays');
+            $table->string('codepays');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
